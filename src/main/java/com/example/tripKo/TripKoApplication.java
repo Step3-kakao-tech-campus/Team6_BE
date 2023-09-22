f06ac1153b1832ac17554ea92c1256d2688d72c3
zigimi <chju0905@pusan.ac.kr>
2023-09-22 18:41:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/tripKo/TripKoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripKo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripKoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripKoApplication.class, args);
+	}
+
+}
