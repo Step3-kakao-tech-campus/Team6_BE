e000769c89176ca8252a2905e079e9ed06478099
zigimi <chju0905@pusan.ac.kr>
2023-09-30 01:15:35 +0900

    refactor: test 코드 분리

--- src/main/java/com/example/tripKo/domain/place/application/PlaceService.java
+++ src/main/java/com/example/tripKo/domain/place/application/PlaceService.java
@@ -1,6 +1,15 @@
 package com.example.tripKo.domain.place.application;
 
+import com.example.tripKo.domain.address.dao.AddressCategoryRepository;
+import com.example.tripKo.domain.address.dao.AddressRepository;
+import com.example.tripKo.domain.address.entity.Address;
+import com.example.tripKo.domain.address.entity.AddressCategory;
+import com.example.tripKo.domain.file.dao.FileRepository;
+import com.example.tripKo.domain.file.entity.File;
+import com.example.tripKo.domain.place.Category;
+import com.example.tripKo.domain.place.dao.PlaceJPARepository;
 import com.example.tripKo.domain.place.dto.PlaceResponseDTO;
+import com.example.tripKo.domain.place.entity.Place;
 import com.example.tripKo.domain.place.entity.PlaceFestival;
 import com.example.tripKo.domain.place.dao.PlaceFestivalJPARepository;
 import com.example.tripKo.domain.place.dto.PlaceFestivalResponseDTO;
@@ -27,13 +36,26 @@ public class PlaceService {
     private final PlaceRestaurantJPARepository placeRestaurantJPARepository;
     private final PlaceFestivalJPARepository placeFestivalJPARepository;
     private final PlaceTouristSpotJPARepository placeTouristSpotJPARepository;
-    @Transactional
-    public PlaceResponseDTO findAllByLocation(String location) {
-//        Place tmp = Place.builder().name("맛도리 짜장").count(10).averageRating(4.1f).summary("개맛도리 추배릅").address("Busan sahagu").build();
+    private final PlaceJPARepository placeJPARepository;
+    private final AddressCategoryRepository addressCategoryRepository;
+    private final AddressRepository addressRepository;
+    private final FileRepository fileRepository;
+
+//    @Transactional
+//    public void test() {
+//        AddressCategory addressCategory = addressCategoryRepository.saveAndFlush(new AddressCategory("hadan", "saha", "Busan"));
+//        Address address = addressRepository.saveAndFlush(Address.builder().buildingName("6공학관").roadName("busan university street").zipCode("11111").addressCategory(addressCategory).build());
+//        File file = fileRepository.saveAndFlush(new File("jpg", "test"));
+//        Place tmp = Place.builder().name("맛도리 짜장").count(10).averageRating(4.1f).summary("완전 맛도리예요!").file(file).address(address).build();
 //        placeJPARepository.saveAndFlush(tmp);
 //        placeRestaurantJPARepository.saveAndFlush(PlaceRestaurant.builder().place(tmp).build());
 //        placeFestivalJPARepository.saveAndFlush(PlaceFestival.builder().endDate("20-12-25").startDate("20-09-20").reservationAvailable(true).place(tmp).build());
 //        placeTouristSpotJPARepository.saveAndFlush(PlaceTouristSpot.builder().place(tmp).build());
+//    }
+
+    @Transactional
+    public PlaceResponseDTO findAllByLocation(String location) {
+//        test();
         List<PlaceRestaurant> placeRestaurants = placeRestaurantJPARepository.findAllByLocation(location);
         List<PlaceFestival> placeFestivals = placeFestivalJPARepository.findAllByLocation(location);
         List<PlaceTouristSpot> placeTouristSpots = placeTouristSpotJPARepository.findAllByLocation(location);
