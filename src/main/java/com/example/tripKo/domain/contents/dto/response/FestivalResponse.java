986b3aefc4a7b26f0a7c8d04bd11ea457b4e48bd
이지민 <min102602@naver.com>
2023-10-04 22:49:03 +0900

    fix : `FestivalResponse` 상세정보 dto 수정

--- src/main/java/com/example/tripKo/domain/contents/dto/response/FestivalResponse.java
+++ src/main/java/com/example/tripKo/domain/contents/dto/response/FestivalResponse.java
@@ -4,7 +4,9 @@ import static lombok.AccessLevel.PRIVATE;
 
 import com.example.tripKo.domain.address.entity.Address;
 import com.example.tripKo.domain.address.entity.AddressCategory;
+import com.example.tripKo.domain.contents.entity.Contents;
 import com.example.tripKo.domain.place.entity.PlaceFestival;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
@@ -20,28 +22,42 @@ public class FestivalResponse {
   private String name;
   private Float averageScore;
   private String mainImage;
-  private List<String> images;
+  private List<Content> contents;
   private String address;
-  private String description;
-  private String imagesPath;
-  private Boolean isWished;
+  private Boolean liked;
   private Boolean isReservable;
   private String period;
+
+  @Builder
+  @Getter
+  public static class Content {
+    private Long page;
+    private String description;
+    private List<String> image;
+  }
 
   public FestivalResponse(PlaceFestival placeFestival) {
     this.id = placeFestival.getId();
     this.name = placeFestival.getPlace().getName();
     this.averageScore = placeFestival.getPlace().getAverageRating();
     this.mainImage = placeFestival.getPlace().getFile().getName();
-    this.images = placeFestival.getPlace().getContents().stream()
-        .flatMap(c -> c.getContentsHasFiles().stream())
-        .map(c -> c.getFile().getName())
+    this.contents = placeFestival.getPlace().getContents().stream()
+        .map(this::mapContent)
         .collect(Collectors.toList());
     this.address = addressToString(placeFestival.getPlace().getAddress());
-    this.description = placeFestival.getPlace().getSummary();
-    this.isWished = false;
+    this.liked = false;
     this.isReservable = placeFestival.getReservationAvailable();
     this.period = placeFestival.getStartDate() + " ~ " + placeFestival.getEndDate();
+  }
+
+  private Content mapContent(Contents contents) {
+    return Content.builder()
+        .page(contents.getPage())
+        .description(contents.getDescription())
+        .image(contents.getContentsHasFiles().stream()
+            .map(c->c.getFile().getName())
+            .collect(Collectors.toList()))
+        .build();
   }
 
   public String addressToString(Address address) {
