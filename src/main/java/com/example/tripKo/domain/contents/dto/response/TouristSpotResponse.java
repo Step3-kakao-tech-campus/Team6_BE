99249a780a8b9c6af2fcbb49165c93f5da319e18
이지민 <min102602@naver.com>
2023-10-04 22:49:54 +0900

    fix : `TouristSpotResponse` 상세정보 dto 수정

--- src/main/java/com/example/tripKo/domain/contents/dto/response/TouristSpotResponse.java
+++ src/main/java/com/example/tripKo/domain/contents/dto/response/TouristSpotResponse.java
@@ -4,6 +4,7 @@ import static lombok.AccessLevel.PRIVATE;
 
 import com.example.tripKo.domain.address.entity.Address;
 import com.example.tripKo.domain.address.entity.AddressCategory;
+import com.example.tripKo.domain.contents.entity.Contents;
 import com.example.tripKo.domain.place.entity.PlaceTouristSpot;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,24 +21,39 @@ public class TouristSpotResponse {
     private String name;
     private Float averageScore;
     private String mainImage;
-    private List<String> images;
+    private List<Content> contents;
     private String address;
-    private String description;
-    private String imagesPath;
     private Boolean isWished;
+
+    @Builder
+    @Getter
+    public static class Content {
+        private Long page;
+        private String description;
+        private List<String> image;
+    }
+
 
     public TouristSpotResponse(PlaceTouristSpot placeTouristSpot) {
         this.id = placeTouristSpot.getId();
         this.name = placeTouristSpot.getPlace().getName();
         this.averageScore = placeTouristSpot.getPlace().getAverageRating();
         this.mainImage = placeTouristSpot.getPlace().getFile().getName();
-        this.images = placeTouristSpot.getPlace().getContents().stream()
-                .flatMap(c -> c.getContentsHasFiles().stream())
-                .map(c -> c.getFile().getName())
-                .collect(Collectors.toList());
+        this.contents = placeTouristSpot.getPlace().getContents().stream()
+            .map(this::mapContent)
+            .collect(Collectors.toList());
         this.address = addressToString(placeTouristSpot.getPlace().getAddress());
-        this.description = placeTouristSpot.getPlace().getSummary();
         this.isWished = false;
+    }
+
+    private Content mapContent(Contents contents) {
+        return Content.builder()
+            .page(contents.getPage())
+            .description(contents.getDescription())
+            .image(contents.getContentsHasFiles().stream()
+                .map(c->c.getFile().getName())
+                .collect(Collectors.toList()))
+            .build();
     }
 
     public String addressToString(Address address) {
