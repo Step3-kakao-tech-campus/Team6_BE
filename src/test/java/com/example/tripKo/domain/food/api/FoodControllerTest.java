020c7788fbc2125675a7f684ed1f1594ff5ac429
이지민 <min102602@naver.com>
2023-10-07 23:59:52 +0900

    test : test 설정 및 restdocs 설정

---
+++ src/test/java/com/example/tripKo/domain/food/api/FoodControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.tripKo.domain.food.api;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import com.example.tripKo.IntegrationTest;
+
+public class FoodControllerTest extends IntegrationTest {
+
+}
