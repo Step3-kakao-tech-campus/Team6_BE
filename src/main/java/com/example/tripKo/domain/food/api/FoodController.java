64a4cac14a1638aed8ea74d9976166bcf27fea9b
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-10-08 15:37:25 +0900

    feat: 음식 상세정보 불러오기 기능 구현
    id 값을 받았을 때 음식의 상세 정보(음식 콘텐츠, 식당, 재료)를 불러오는 기능을 구현하였습니다.

--- src/main/java/com/example/tripKo/domain/food/api/FoodController.java
+++ src/main/java/com/example/tripKo/domain/food/api/FoodController.java
@@ -2,11 +2,13 @@ package com.example.tripKo.domain.food.api;
 
 import com.example.tripKo._core.utils.ApiUtils;
 import com.example.tripKo.domain.food.application.FoodService;
+import com.example.tripKo.domain.food.dto.response.FoodDetailsResponse;
 import com.example.tripKo.domain.food.dto.response.FoodResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,13 @@ public class FoodController {
     @GetMapping("/foods")
     public ResponseEntity<?> findByKeyword(@RequestParam(value = "query", defaultValue = "") String query, @RequestParam(value = "sort", defaultValue = "True") String sort) {
         List<FoodResponse> responseDTO = foodService.findByKeyword(query, sort);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
+    }
+
+    @GetMapping("/foods/{foodId}")
+    public ResponseEntity<?> findByFoodId(@PathVariable Long foodId) {
+        FoodDetailsResponse responseDTO = foodService.getFoodInfo(foodId);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
     }
