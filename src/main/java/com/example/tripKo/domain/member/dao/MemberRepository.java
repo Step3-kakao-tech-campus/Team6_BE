ad65ef0639b8153674c7e2c2c5bcb645a96e8aec
zigimi <chju0905@pusan.ac.kr>
2023-10-20 12:33:53 +0900

    feat: findByMemberId 생성

--- src/main/java/com/example/tripKo/domain/member/dao/MemberRepository.java
+++ src/main/java/com/example/tripKo/domain/member/dao/MemberRepository.java
@@ -3,6 +3,9 @@ package com.example.tripKo.domain.member.dao;
 import com.example.tripKo.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByMemberId(String memberId);
 }
