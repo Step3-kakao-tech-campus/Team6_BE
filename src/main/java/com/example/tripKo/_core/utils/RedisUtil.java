83e566da24d0ca48309ed1360f1842d5b9c8e7de
이지민 <min102602@naver.com>
2023-10-21 11:58:41 +0900

    feat : `RedisUtil` 추가

---
+++ src/main/java/com/example/tripKo/_core/utils/RedisUtil.java
@@ -0,0 +1,31 @@
+package com.example.tripKo._core.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Duration;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+  private final StringRedisTemplate redisTemplate;
+
+  public String getData(String key) {
+    ValueOperations<String , String> valueOperations = redisTemplate.opsForValue();
+    return valueOperations.get(key);
+  }
+
+  public void setDataExpire(String key, String value, long duration) {
+    ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+    Duration expireDuration = Duration.ofMillis(duration);
+    valueOperations.set(key, value, expireDuration);
+  }
+
+  public void deleteData(String key) {
+    redisTemplate.delete(key);
+  }
+}
