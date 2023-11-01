25718a69edec8c10db0679f2789d47d11212930b
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-11-01 12:15:21 +0900

    refactor: 수정된 음식 상세정보 API에 맞게 클래스 수정, 삭제
    FoodContents를 삭제하고 Food에 음식 사진과 설명을 담을 수 있게 수정하였습니다.

---
+++ src/main/java/com/example/tripKo/domain/food/dao/FoodHasFileRepository.java
@@ -0,0 +1,7 @@
+package com.example.tripKo.domain.food.dao;
+
+import com.example.tripKo.domain.food.entity.FoodHasFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodHasFileRepository extends JpaRepository<FoodHasFile, Long> {
+}
