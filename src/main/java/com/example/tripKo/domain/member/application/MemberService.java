b968518ac118e15b511e6d35f7f9de02355cab4e
zigimi <chju0905@pusan.ac.kr>
2023-11-08 21:48:42 +0900

    feat: 프로필 이미지 변경 api

--- src/main/java/com/example/tripKo/domain/member/application/MemberService.java
+++ src/main/java/com/example/tripKo/domain/member/application/MemberService.java
@@ -1,8 +1,11 @@
 package com.example.tripKo.domain.member.application;
 
+import com.example.tripKo._core.errors.exception.Exception400;
 import com.example.tripKo._core.errors.exception.Exception404;
+import com.example.tripKo._core.errors.exception.Exception500;
 import com.example.tripKo._core.security.JwtProvider;
 import com.example.tripKo._core.security.data.JwtToken;
+import com.example.tripKo.domain.file.dao.FileRepository;
 import com.example.tripKo.domain.member.MemberReservationStatus;
 import com.example.tripKo.domain.member.MemberRoleType;
 import com.example.tripKo.domain.member.application.convenience.CheckDuplicateService;
@@ -15,7 +18,12 @@ import com.example.tripKo.domain.member.dto.response.RestaurantReservationRespon
 import com.example.tripKo.domain.member.dto.response.userInfo.UserInfoResponse;
 import com.example.tripKo.domain.member.entity.Member;
 import com.example.tripKo.domain.member.entity.MemberReservationInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.example.tripKo.domain.place.dao.PlaceFestivalRepository;
@@ -38,6 +46,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -53,6 +62,7 @@ public class MemberService {
   private final PasswordEncoder passwordEncoder;
   private final AuthenticationManagerBuilder authenticationManagerBuilder;
   private final CheckDuplicateService checkDuplicateService;
+  private final FileRepository fileRepository;
 
   @Transactional
   public UserInfoResponse getUserInfo(Member member) {
@@ -69,6 +79,16 @@ public class MemberService {
     }
     member.updateUserInfo(userInfoRequest);
     memberRepository.save(member);
+  }
+
+  @Transactional
+  public void setUserInfoImage(Member member, MultipartFile image) {
+    com.example.tripKo.domain.file.entity.File file = saveImages(image);
+    file = fileRepository.save(file);
+
+    member.updateFile(file);
+    memberRepository.save(member);
+
   }
 
   @Transactional
@@ -196,4 +216,59 @@ public class MemberService {
       throw new Exception404("동일한 아이디가 존재합니다.");
     }
   }
+
+  private com.example.tripKo.domain.file.entity.File saveImages(MultipartFile image) {
+    //이미지가 저장될 경로는 /src/main/resources/reviews/images/
+    String imagesPath = new File("").getAbsolutePath() + File.separator
+            + "src" + File.separator
+            + "main" + File.separator
+            + "resources" + File.separator
+            + "reviews" + File.separator
+            + "images";
+
+    File imageFile = new File(imagesPath);
+
+    //resources/review/images에 디렉토리 생성
+    if (!imageFile.exists()) {
+      boolean isExists = imageFile.mkdirs();
+      if (!isExists) {
+        throw new Exception500("경로 생성에 실패하였습니다.");
+      }
+    }
+
+    String contentType = image.getContentType();
+    String imageName = image.getOriginalFilename();
+    String fileExtension;
+
+    //jpeg, jpg, png만 허용
+    if (Objects.isNull(contentType)) {
+      throw new Exception400("올바르지 않은 파일 확장자 형식입니다.");
+    }
+    else if (contentType.contains("image/jpeg") ||
+            contentType.contains("image/jpg"))
+      fileExtension = ".jpg";
+    else if (contentType.contains("image/png"))
+      fileExtension = ".png";
+    else throw new Exception400("올바르지 않은 파일 확장자 형식입니다.");
+
+    String newFileName = String.format("%1$016x",System.nanoTime()) + "_" + imageName;
+    System.out.println("====================");
+    System.out.println(newFileName);
+
+    com.example.tripKo.domain.file.entity.File fileEntity = com.example.tripKo.domain.file.entity.File.builder()
+            .type(contentType)
+            .name(newFileName)
+            .build();
+
+    String imagePath = imagesPath + File.separator + newFileName;
+    File savedImage = new File(imagePath);
+    try {
+      image.transferTo(savedImage);
+    } catch (IOException e) {
+      throw new Exception500("이미지를 저장하는 중 문제가 발생하였습니다.");
+    }
+
+    return fileEntity;
+  }
+
 }
