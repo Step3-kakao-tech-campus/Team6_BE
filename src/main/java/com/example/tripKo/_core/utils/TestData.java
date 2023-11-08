6b8bab68a6952d4d6229c29096c94dfe5647018a
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-11-09 02:54:26 +0900

    build: 배포 환경으로 변경

--- src/main/java/com/example/tripKo/_core/utils/TestData.java
+++ src/main/java/com/example/tripKo/_core/utils/TestData.java
@@ -43,7 +43,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
-@Profile("test")
+@Profile("prod")
 @RequiredArgsConstructor
 public class TestData implements CommandLineRunner {
     private final PlaceRepository placeRepository;
