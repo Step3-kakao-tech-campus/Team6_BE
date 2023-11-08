e2b03674f0d04644d8e2b832d27598bccfa3692b
zigimi <chju0905@pusan.ac.kr>
2023-11-08 21:03:06 +0900

    fix: 위시리스트 - 멤버 간 양방향 매핑

--- src/main/java/com/example/tripKo/domain/member/entity/Member.java
+++ src/main/java/com/example/tripKo/domain/member/entity/Member.java
@@ -10,16 +10,7 @@ import com.example.tripKo.domain.file.entity.File;
 import com.example.tripKo.domain.member.MemberRoleType;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.example.tripKo.domain.member.dto.request.userInfo.UserInfoRequest;
 import lombok.AllArgsConstructor;
@@ -86,6 +77,10 @@ public class Member extends BaseTimeEntity {
 
   @OneToMany(mappedBy = "member")
   private final List<MemberReservationInfo> memberReservationInfos = new ArrayList<>();
+
+  @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+  private final List<MemberHasPlaceIsWished> memberHasPlaceIsWishedList = new ArrayList<>();
+
 
   @Builder
   public Member(MemberRoleType role, String realName, String nickName, String emailAddress, String memberId,
