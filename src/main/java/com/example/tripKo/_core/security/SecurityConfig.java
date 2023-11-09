b949ef2918142f0312f9c60ba571c23731586870
manjae98 <manjae98@naver.com>
2023-11-09 22:20:19 +0900

    fix: cors origin 수정

--- src/main/java/com/example/tripKo/_core/security/SecurityConfig.java
+++ src/main/java/com/example/tripKo/_core/security/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.addAllowedHeader("*");
     configuration.addAllowedMethod("*");
-    configuration.addAllowedOriginPattern("https://kd284b475c6e5a.user-app.krampoline.com");
+    configuration.addAllowedOrigin("https://kd284b475c6e5a.user-app.krampoline.com");
     configuration.addExposedHeader("authorization");
     configuration.addExposedHeader("refreshToken");
     configuration.setAllowCredentials(true);
