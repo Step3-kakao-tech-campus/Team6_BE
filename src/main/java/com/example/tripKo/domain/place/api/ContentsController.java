a27fe98ef03c0c4ea8642baaadfbeef953c083b6
manjae98 <manjae98@naver.com>
2023-11-10 00:38:50 +0900

    fix: origin import 추가

--- src/main/java/com/example/tripKo/domain/place/api/ContentsController.java
+++ src/main/java/com/example/tripKo/domain/place/api/ContentsController.java
@@ -17,6 +17,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Validated
 @RestController
