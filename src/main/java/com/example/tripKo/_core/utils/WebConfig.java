ab857168207cd347390fbb93c834f063a23fde17
manjae98 <manjae98@naver.com>
2023-11-09 20:58:33 +0900

    conf: cors webconfig 클래스 생성

---
+++ src/main/java/com/example/tripKo/_core/utils/WebConfig.java
@@ -0,0 +1,8 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        	.allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://localhost:80") // 허용할 출처
+    }
+}
