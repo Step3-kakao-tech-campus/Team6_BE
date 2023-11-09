ab5fadc2c08e3cb000fb407edaabc8f6a83ac97f
manjae98 <manjae98@naver.com>
2023-11-09 21:04:21 +0900

    fix: WebConfig 오타 수정

--- src/main/java/com/example/tripKo/_core/utils/WebConfig.java
+++ src/main/java/com/example/tripKo/_core/utils/WebConfig.java
@@ -1,8 +1,14 @@
+package com.example.tripKo._core.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        	.allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://localhost:80"); // 허용할 출처
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://localhost:80"); // 허용할 출처
     }
 }
