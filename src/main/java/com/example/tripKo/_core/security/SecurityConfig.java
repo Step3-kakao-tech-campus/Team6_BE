ce572ade1d64613b1fc6701c56a1d68ecfd97368
manjae98 <manjae98@naver.com>
2023-11-09 23:01:03 +0900

    fix: ip 변경

--- src/main/java/com/example/tripKo/_core/security/SecurityConfig.java
+++ src/main/java/com/example/tripKo/_core/security/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.addAllowedHeader("*");
     configuration.addAllowedMethod("*");
-    configuration.addAllowedOrigin("http://10.95.36.162:3000");
+    configuration.addAllowedOrigin("http://10.231.7.98:3000");
     configuration.addExposedHeader("authorization");
     configuration.addExposedHeader("refreshToken");
     configuration.setAllowCredentials(true);
