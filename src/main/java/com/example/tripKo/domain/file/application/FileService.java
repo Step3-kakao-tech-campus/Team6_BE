92ca4bde43185932aaef76a99c1bfdc9bd4298a1
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-11-11 02:19:42 +0900

    refactor: file init 메서드 수정

--- src/main/java/com/example/tripKo/domain/file/application/FileService.java
+++ src/main/java/com/example/tripKo/domain/file/application/FileService.java
@@ -3,6 +3,10 @@ package com.example.tripKo.domain.file.application;
 import com.amazonaws.util.IOUtils;
 import com.example.tripKo._core.S3.ImageS3Service;
 import com.example.tripKo._core.errors.exception.Exception500;
+import com.example.tripKo.domain.file.dao.FileRepository;
+import com.example.tripKo.domain.file.dto.FileRequest;
+import com.example.tripKo.domain.file.entity.File;
+import com.example.tripKo.domain.place.entity.ReviewHasFile;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
@@ -12,30 +16,23 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class FileService {
     private final ImageS3Service imageS3Service;
+    private final FileRepository fileRepository;
 
-    public boolean initImages() {
-        File file = new File(new File("").getAbsolutePath() + "image/restaurant/CocamomeMangmi/main.png");
-        FileItem fileItem;
+    public boolean initImages(FileRequest fileRequest) {
+        if (!fileRequest.getImage().isEmpty()) {
+            List<File> fileEntities = new ArrayList<>();
+            for(MultipartFile i : fileRequest.getImage()) {
+                fileEntities.add(imageS3Service.uploadImage(i));
+            }
 
-        try {
-            fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
-            InputStream input = new FileInputStream(file);
-            OutputStream os = fileItem.getOutputStream();
-            IOUtils.copy(input, os);
-            // Or faster..
-            // IOUtils.copy(new FileInputStream(file), fileItem.getOutputStream());
-
-            //jpa.png -> multipart 변환
-            MultipartFile  mFile = new CommonsMultipartFile(fileItem);
-            imageS3Service.uploadImage(mFile);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            throw new Exception500("이미지 저장을 실패하였습니다.");
+            fileRepository.saveAll(fileEntities);
         }
 
         return true;
