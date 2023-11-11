d71ee762b9197869d50091ecaf52d9a24434fe9e
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-11-11 10:58:58 +0900

    refactor: proxy 설정

--- src/main/java/com/example/tripKo/_core/S3/ImageS3Service.java
+++ src/main/java/com/example/tripKo/_core/S3/ImageS3Service.java
@@ -1,20 +1,27 @@
 package com.example.tripKo._core.S3;
 
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.SdkHttpUtils;
 import com.example.tripKo._core.errors.ErrorCode;
 import com.example.tripKo._core.errors.exception.BusinessException;
 import com.example.tripKo._core.errors.exception.Exception400;
 import com.example.tripKo._core.errors.exception.Exception404;
 import com.example.tripKo._core.errors.exception.Exception500;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHost;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
@@ -30,12 +37,49 @@ public class ImageS3Service{
 
     @Value("${cloud.aws.region.static}")
     private String region;
+
+    @Value("${spring.devtools.remote.proxy.host}")
+    private String proxyHost;
+
+    @Value("${spring.devtools.remote.proxy.port}")
+    private int proxyPort;
+
     private String changedImageName(String originName) { //이미지 이름 중복 방지를 위해 랜덤으로 생성
         String random = UUID.randomUUID().toString();
         return random+originName;
     }
 
+    private AmazonS3 createAmazonS3ClientWithProxy() {
+        // Create proxy settings
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+
+        // Configure the client with proxy settings
+        try {
+            return AmazonS3ClientBuilder.standard()
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(region, region))
+                    .withPathStyleAccessEnabled(true)
+                    .withClientConfiguration(getClientConfigurationWithProxy(proxy))
+                    .build();
+        } catch (SdkClientException e) {
+            // Handle exception
+            throw new RuntimeException("Error creating S3 client with proxy.", e);
+        }
+    }
+
+    private com.amazonaws.ClientConfiguration getClientConfigurationWithProxy(Proxy proxy) {
+        com.amazonaws.ClientConfiguration clientConfiguration = new com.amazonaws.ClientConfiguration();
+        if (proxy.type() == Proxy.Type.HTTP) {
+            InetSocketAddress address = (InetSocketAddress) proxy.address();
+            HttpHost httpHost = new HttpHost(address.getHostName(), address.getPort());
+            clientConfiguration.setProxyHost(httpHost.getHostName());
+            clientConfiguration.setProxyPort(httpHost.getPort());
+        }
+        return clientConfiguration;
+    }
+
     public String uploadImageToS3(MultipartFile image) { //이미지를 S3에 업로드하고 이미지의 url을 반환
+        AmazonS3 s3clientWithProxy = createAmazonS3ClientWithProxy();
+
         String originName = image.getOriginalFilename(); //원본 이미지 이름
         String contentType = image.getContentType(); //확장자
         String fileExtension;
@@ -55,14 +99,14 @@ public class ImageS3Service{
         ObjectMetadata metadata = new ObjectMetadata(); //메타데이터
         metadata.setContentType("image/"+fileExtension);
         try {
-            PutObjectResult putObjectResult = amazonS3.putObject(new PutObjectRequest(
+            PutObjectResult putObjectResult = s3clientWithProxy.putObject(new PutObjectRequest(
                     bucketName, "images/" + changedName, image.getInputStream(), metadata
             ).withCannedAcl(CannedAccessControlList.PublicRead));
 
         } catch (IOException e) {
             throw new BusinessException(originName, "file name", ErrorCode.IMAGE_CANNOT_SAVE);
         }
-        return amazonS3.getUrl(bucketName, "images/" + changedName).toString(); //데이터베이스에 저장할 이미지가 저장된 주소
+        return s3clientWithProxy.getUrl(bucketName, "images/" + changedName).toString(); //데이터베이스에 저장할 이미지가 저장된 주소
 
     }
 
